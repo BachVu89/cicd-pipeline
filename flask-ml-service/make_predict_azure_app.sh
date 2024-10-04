88b5e196fd9ee04487838c5e236d94d5e7aff89b
hoangbach89gl <hoangbach89gl@gmail.com>
2024-10-04 18:22:43 +0700

    update file app.sh

--- flask-ml-service/make_predict_azure_app.sh
+++ flask-ml-service/make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flaskapp1803.azurewebsites.net:$PORT/predict 
+     -X POST https://udacity2.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
