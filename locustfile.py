ca8440625827135c179a5d9e38563436772e3ebb
hoangbach89gl <hoangbach89gl@gmail.com>
2024-10-28 09:53:49 +0700

    update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -7,8 +7,16 @@ class WebsiteTestUser(HttpUser):
             "LSTAT": {"0": 4.98}}
     @task(1)
     def test1(self):
-        self.client.get("https://udacity2.azurewebsites.net/")
+        with self.client.get("https://udacity2.azurewebsites.net/", catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure("Failed to load page")
+            else:
+                print(f"GET response time: {response.elapsed.total_seconds()} seconds")
 
     @task(2)
     def test2(self):
-        self.client.post("https://udacity2.azurewebsites.net:443/predict", json=self.data)
+        with self.client.post("https://udacity2.azurewebsites.net:443/predict", json=self.data, catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure("Failed to post data")
+            else:
+                print(f"POST response time: {response.elapsed.total_seconds()} seconds")
